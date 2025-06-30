941d59d540c82fb6034d003acc36576269b377af
rye <lepyrogang@proton.me>
2025-06-30 14:04:32 +0330

    fix: updated sway config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,7 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-set fish_greeting ""
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
