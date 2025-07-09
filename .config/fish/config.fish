7789f190d5ab054ec2cd71396aaafd338e301325
rye <lepyrogang@proton.me>
2025-07-09 15:54:43 +0330

    fix: symlinked the whole .config folder, i am so smart muhahahahah

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,4 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
-
 # overwrite greeting
 # potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+function fish_greeting
+end
